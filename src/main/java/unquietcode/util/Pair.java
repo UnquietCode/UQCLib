27b0aceeff6e07cdf3c1b87c74335001118b6ce5
Benjamin Fagin <blous@unquietcode.com>
2011-02-05 11:39:27 -0600

    UQCLIB-8: Pair class...what's up? Added the comparators and a simple typed test.

--- src/main/java/unquietcode/util/Pair.java
+++ src/main/java/unquietcode/util/Pair.java
@@ -1,9 +1,31 @@
+/*******************************************************************************
+ Copyright 2009-2011 Benjamin Fagin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+
+ Read the included LICENSE.TXT for more information.
+ ******************************************************************************/
+
 package unquietcode.util;
 
+import java.util.Comparator;
+
 /**
- * @author Ben
- * @version 1.0
- * Date: Nov 25, 2010
+ * @author  Benjamin Fagin
+ * @version Nov 25, 2010
+ *
+ *
  */
 public class Pair<A, B> {
 	public A first;
@@ -14,6 +36,38 @@ public class Pair<A, B> {
 		this.second = second;
 	}
 
+	// JavaBeans properties
+	public A getFirst() {
+		return first;
+	}
+
+	public void setFirst(A value) {
+		first = value;
+	}
+
+	public B getSecond() {
+		return second;
+	}
+
+	public void setSecond(B value) {
+		second = value;
+	}
+
+	// comparators
+	public static class Comparator1to1<T extends Comparable<T>> implements Comparator<Pair<T,?>> {
+		public int compare(Pair<T, ?> p1, Pair<T, ?> p2) {
+			return p1.first.compareTo(p2.first);
+		}
+	}
+
+	public static class Comparator2to2<T extends Comparable<T>> implements Comparator<Pair<?,T>> {
+		public int compare(Pair<?, T> p1, Pair<?, T> p2) {
+			return p1.second.compareTo(p2.second);
+		}
+	}
+
+
+	// named static classes //
 	public static class NameValue<C, D> {
 		public C name;
 		public D value;
---
+++ src/test/java/unquietcode/util/Pair_T.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ Copyright 2009-2011 Benjamin Fagin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+
+ Read the included LICENSE.TXT for more information.
+ ******************************************************************************/
+
+package unquietcode.util;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author  Benjamin Fagin
+ * @version 02-05-2011
+ */
+@SuppressWarnings("unchecked")
+public class Pair_T {
+	@Test
+	public void sort() {
+		Pair<Integer, String> p1 = new Pair<Integer, String>(5, "good");
+		Pair<Integer, String> p2 = new Pair<Integer, String>(1, "morning");
+		Pair<Integer, String> p3 = new Pair<Integer, String>(7, "to");
+		Pair<Integer, String> p4 = new Pair<Integer, String>(4, "you");
+
+		Pair<String, String> p5 = new Pair<String, String>("is", "different");
+
+		Pair<Integer, String> pairs[] = new Pair[]{p4, p2, p1, p3};
+		Arrays.sort(pairs, new Pair.Comparator1to1<Integer>());
+
+		for (Pair p : pairs) {
+			System.out.println(p.second);
+		}
+
+		System.out.println();
+
+		ArrayList<Pair<?, String>> plist = new ArrayList<Pair<?, String>>();
+		plist.addAll(Arrays.asList(pairs));
+		plist.add(p5);
+		Collections.sort(plist, new Pair.Comparator2to2<String>());
+
+		for (Pair p : plist) {
+			System.out.println(p.second);
+		}
+
+	}
+}
